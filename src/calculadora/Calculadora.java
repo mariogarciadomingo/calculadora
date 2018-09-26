15910983993c313a1e08fc83740ff1d29b944c7f
mario <mario@DESKTOP-VIB8HDB>
2018-09-26 16:26:41 +0200

    creació del mètode suma

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -3,16 +3,19 @@ package calculadora;
 import java.util.Scanner;
 
 public class Calculadora {
-
-
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+       Scanner in = new Scanner(System.in);
        int num1 = 0;
        int num2 = 0;
-       System.out.print("introdueix el primer nombre");
+       System.out.println("introdueix el primer nombre");
        num1=in.nextInt();
-       System.out.print("introdueix el segon nombre");
+       System.out.println("introdueix el segon nombre");
        num2=in.nextInt();
+       suma(num1,num2);
     }
-    
+    private static int suma(int x,int y)
+    {
+        System.out.println(x+"+"+y+"="+(x+y));
+        return x+y;
+    }
 }
