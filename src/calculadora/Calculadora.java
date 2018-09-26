cad665ce5ee88e3f500616cc9413380066a89e69
mario <mario@DESKTOP-VIB8HDB>
2018-09-26 16:21:10 +0200

    creació de la introducció de dades

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,9 +1,18 @@
 package calculadora;
+
+import java.util.Scanner;
+
 public class Calculadora {
 
 
     public static void main(String[] args) {
-       
+        Scanner in = new Scanner(System.in);
+       int num1 = 0;
+       int num2 = 0;
+       System.out.print("introdueix el primer nombre");
+       num1=in.nextInt();
+       System.out.print("introdueix el segon nombre");
+       num2=in.nextInt();
     }
     
 }
